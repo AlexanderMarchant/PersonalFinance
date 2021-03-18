394c6e22ef57c2ed2b823c32fe4aebeee71a34bd
AlexanderMarchant <alex_marchant@outlook.com>
2021-03-18 18:00:00 +0000

    Initial Commit

---
+++ PersonalFinance/PersonalFinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalFinanceApp.swift
+//  PersonalFinance
+//
+//  Created by Alex Marchant on 18/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct PersonalFinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
